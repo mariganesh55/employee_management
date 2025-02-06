093dd79344e670557975f26ae1870b995bbf0cca
mariganesh55 <mariganeshthangam@gmail.com>
2025-02-06 13:43:47 +0530

    Deploy Flutter Web

--- lib/data/cubit/employee_management/employee_management_state.dart
+++
@@ -1,7 +0,0 @@
-part of 'employee_management_cubit.dart';
-
-abstract class EmployeeManagementState extends BaseEquatable {}
-
-class EmployeeManagementInitial extends EmployeeManagementState {}
-
-class EmployeeManagementRefreshState extends EmployeeManagementState {}
--- lib/utils/color_resource.dart
+++
@@ -1,5 +0,0 @@
-import 'dart:ui';
-
-class ColorResource {
-  static const Color grey = Color(0xffE5E5E5);
-}
